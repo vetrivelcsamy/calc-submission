6bf0fca9a98b13a2deab3909d9fb3920bbaaf017
vetrivel chinnasamy <26738977+vetrivelcsamy@users.noreply.github.com>
2019-06-02 12:47:48 +0530

    innertext buttons problem solved

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
-const button = document.querySelector('button');
+let runningTotal = 0
+let buffer = "0"
+let previousOperator
+const screen = document.querySelector("#screen")
 
-document.querySelector('button').addEventListener('click', event => {
-	    //alert(`You clicked on button ${event.target.innerText}`);
-	    // console.log(`You clicked on button ${event.target.innerText}`);
-     //button.innerHTML = `Click count: ${event.detail}`;
-     document.getElementById("screen").innerHTML += "!";
-});
+document.querySelector(".calc-buttons").addEventListener("click", function(event) {
+	buttonClick(event.target.innerText)
+})
+
+function buttonClick(value) {
+	if (isNaN(parseInt(value))) {
+		handleSymbol(value)
+	} else {
+		handleNumber(value)
+	}
+}
+
+function handleNumber(value) {
+	if (buffer === "0") {
+		buffer = value
+	} else {
+		buffer += value
+	}
+	rerender()
+}
+
+function rerender() {
+	screen.innerText = buffer
+}
